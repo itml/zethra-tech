3106715c012029ba9c2bba412735833b012c2f0a
zethra <jediben97@gmail.com>
2016-03-30 03:52:51 +0000

    Added link to mao to minecraft server page

--- minecraft.php
+++ minecraft.php
@@ -38,8 +38,10 @@ $Timer = Number_Format(MicroTime(true) - $Timer, 4, '.', '');
     <div style="text-align: center;">
         <h2>Zethra's Minecraft Server</h2>
 
-        <h2> IP is: 162.243.4.46</h2>
+        <h2>IP is: 162.243.4.46</h2>
 
+	<h2><a href="http://map.zethratech.com" target="_blank">Server Map</a></h2>
+	
         <?php
         $Info = $Query->GetInfo();
         if (isset($Exception)) {
